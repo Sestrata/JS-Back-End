2af0410c47366b3cba573622abda6f708a9a323f
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-11-16 12:32:32 +0200

    Create database.js

---
+++ Tutorials/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://127.0.0.1:27017/tutorialsdb';
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database connected');
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+};
